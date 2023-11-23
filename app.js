20f29aeb2d087d6ab0c166fd866d2c989691a8d7
Kinaryo <kinaryo733huda@gmail.com>
2023-11-24 04:40:53 +0700

    membuat routes agar memoermudah membaca file

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const ejsMate = require('ejs-mate')
 const express = require('express')
 const ErrorHandler= require('./utils/ErrorHandler')
-const Joi = require('joi')
 const methodOverride = require('method-override')
 const path = require('path')
 const app = express();
@@ -10,12 +9,7 @@ const wrapAsync = require('./utils/wrapAsync')
 
 
 
-// stage 2 penambahan models 
-const Place = require('./models/place');
-const Review = require('./models/review')
 
-const {placeSchema} = require('./schemas/place')
-const {reviewSchema} =require('./schemas/review')
 
 // function connect to mongodb
 mongoose.connect('mongodb://127.0.0.1/bestpoints').then((result)=>{
@@ -28,7 +22,7 @@ app.engine('ejs',ejsMate)
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
-
+ 
 
 //mildware untuk mendapatkan data dari body;
 app.use(express.urlencoded({extended:true}))
@@ -36,89 +30,17 @@ app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
 
 
-const validatePlace = (req,res,next) =>{
-    const {error}= placeSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(el=>el.message).join(',')
-        return next(new ErrorHandler(msg,400))
-    }else{
-        next();
-    }
-}
-
-const validateReview = (req,res,next) =>{
-    const {error}= reviewSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map(el=>el.message).join(',')
-        return next(new ErrorHandler(msg,400))
-    }else{
-        next();
-    }
-}
-
-
-
 
 
 app.get('/',(req,res)=>{
     res.render('home')
 })
+app.use('/places',require('./routes/places'))
+app.use('/places/:place_id/reviews', require('./routes/reviews'))
 
-app.get('/places', wrapAsync (async (req, res) => {
-    const places = await Place.find();
-    res.render('places/index', { places });
-}))
-
-app.get('/places/create',(req,res)=>{
-    res.render('places/create')
-})
-
-app.post('/places',validatePlace, wrapAsync (async(req,res,next)=>{
-    const place = new Place(req.body.place)
-    await place.save();
-    res.redirect('/places')
-}))
-
-app.get('/places/:id',wrapAsync (async(req,res)=>{
-    const {id} = req.params
-    const place = await Place.findById(id).populate('reviews')
-    res.render('places/show',{place})
-}))
-
-app.get('/places/:id/edit',wrapAsync (async(req,res)=>{
-        const {id} = req.params
-    const place = await Place.findById(id)
-    res.render('places/edit',{place})
-}))
-
-app.put('/places/:id',validatePlace, wrapAsync (async(req,res)=>{
-const place = await Place.findByIdAndUpdate(req.params.id,{...req.body.place})
-res.redirect('/places')
-}))
-
-
-app.delete('/places/:id', wrapAsync (async (req,res)=>{
-    await Place.findByIdAndDelete(req.params.id)
-    res.redirect('/places')
-}))
 
 // comments iniiiiiii
-app.post('/places/:id/reviews',validateReview, wrapAsync(async (req,res)=>{
-    const review = new Review(req.body.review);
-    const place = await Place.findById(req.params.id)
-    place.reviews.push(review)
-    await review.save();
-    await place.save()
-    res.redirect(`/places/${req.params.id}`)
-}))
 
-// hapus koment 
-app.delete('/places/:place_id/reviews/:review_id', wrapAsync(async(req,res)=>{
-    const {place_id,review_id} = req.params;
-    await Place.findByIdAndUpdate(place_id,{$pull: {reviews: { _id:review_id}}});
-    await Review.findByIdAndDelete(review_id)
-    res.redirect(`/places/${place_id}`);
-}))
 
 
 
