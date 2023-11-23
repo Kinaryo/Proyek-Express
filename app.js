7c648271e47564ae7ba1683c023debdae41b8b30
Kinaryo <kinaryo733huda@gmail.com>
2023-11-24 03:27:29 +0700

    menambhkan bagian error validasi dam validasi data input serta modul review

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const ejsMate = require('ejs-mate')
 const express = require('express')
 const ErrorHandler= require('./utils/ErrorHandler')
+const Joi = require('joi')
 const methodOverride = require('method-override')
 const path = require('path')
 const app = express();
@@ -11,6 +12,10 @@ const wrapAsync = require('./utils/wrapAsync')
 
 // stage 2 penambahan models 
 const Place = require('./models/place');
+const Review = require('./models/review')
+
+const {placeSchema} = require('./schemas/place')
+const {reviewSchema} =require('./schemas/review')
 
 // function connect to mongodb
 mongoose.connect('mongodb://127.0.0.1/bestpoints').then((result)=>{
@@ -30,6 +35,31 @@ app.use(express.urlencoded({extended:true}))
 // untuk mengubah method post menjadi method yang kita ingkinkan melalui query parameter 
 app.use(methodOverride('_method'));
 
+
+const validatePlace = (req,res,next) =>{
+    const {error}= placeSchema.validate(req.body)
+    if(error){
+        const msg = error.details.map(el=>el.message).join(',')
+        return next(new ErrorHandler(msg,400))
+    }else{
+        next();
+    }
+}
+
+const validateReview = (req,res,next) =>{
+    const {error}= reviewSchema.validate(req.body)
+    if(error){
+        const msg = error.details.map(el=>el.message).join(',')
+        return next(new ErrorHandler(msg,400))
+    }else{
+        next();
+    }
+}
+
+
+
+
+
 app.get('/',(req,res)=>{
     res.render('home')
 })
@@ -43,20 +73,15 @@ app.get('/places/create',(req,res)=>{
     res.render('places/create')
 })
 
-app.post('/places', wrapAsync (async(req,res,next)=>{
-    try{
+app.post('/places',validatePlace, wrapAsync (async(req,res,next)=>{
     const place = new Place(req.body.place)
     await place.save();
     res.redirect('/places')
-    }
-    catch (error){
-        next(error)
-    }
 }))
 
 app.get('/places/:id',wrapAsync (async(req,res)=>{
     const {id} = req.params
-    const place = await Place.findById(id)
+    const place = await Place.findById(id).populate('reviews')
     res.render('places/show',{place})
 }))
 
@@ -66,7 +91,7 @@ app.get('/places/:id/edit',wrapAsync (async(req,res)=>{
     res.render('places/edit',{place})
 }))
 
-app.put('/places/:id', wrapAsync (async(req,res)=>{
+app.put('/places/:id',validatePlace, wrapAsync (async(req,res)=>{
 const place = await Place.findByIdAndUpdate(req.params.id,{...req.body.place})
 res.redirect('/places')
 }))
@@ -76,6 +101,28 @@ app.delete('/places/:id', wrapAsync (async (req,res)=>{
     await Place.findByIdAndDelete(req.params.id)
     res.redirect('/places')
 }))
+
+// comments iniiiiiii
+app.post('/places/:id/reviews',validateReview, wrapAsync(async (req,res)=>{
+    const review = new Review(req.body.review);
+    const place = await Place.findById(req.params.id)
+    place.reviews.push(review)
+    await review.save();
+    await place.save()
+    res.redirect(`/places/${req.params.id}`)
+}))
+
+// hapus koment 
+app.delete('/places/:place_id/reviews/:review_id', wrapAsync(async(req,res)=>{
+    const {place_id,review_id} = req.params;
+    await Place.findByIdAndUpdate(place_id,{$pull: {reviews: { _id:review_id}}});
+    await Review.findByIdAndDelete(review_id)
+    res.redirect(`/places/${place_id}`);
+}))
+
+
+
+
 
 app.all('*',(req,res,next)=>{
     next(new ErrorHandler('page not found', 404))
