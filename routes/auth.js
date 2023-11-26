a5e7c694b394be472603bc11004ec97b2b57e620
Kinaryo <kinaryo733huda@gmail.com>
2023-11-27 04:42:05 +0700

    Memisahkan Logic Dan Route Review Juga Auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user')
+const AuthController = require('../controllers/auth')
 const wrapAsync = require('../utils/wrapAsync');
 const passport = require('passport');
 
-router.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
+router.get('/register',AuthController.registerForm)
 
-router.post('/register',wrapAsync(async(req,res)=>{
-    try{
-        const {email,username,password} = req.body;
-        const user = new User({email,username});
-       const registerUser = await User.register(user,password);
+router.post('/register',wrapAsync(AuthController.register))
 
-       req.login(registerUser,(err)=>{
-        if(err) return next(err);
-
-        req.flash('success_msg',"You are registred and can log in ")
-        res.redirect('/places')
-       })
-   }catch(error){
-    req.flash('error_msg', error.message)
-    res.redirect('/register')
-   }
-}))
-
-
-router.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
+router.get('/login', AuthController.loginForm)
 
 router.post('/login', passport.authenticate('local',{
     failureRedirect:'/login',
@@ -37,19 +17,9 @@ router.post('/login', passport.authenticate('local',{
         type:'error_msg',
         msg:'Invalid username or password'
     }
-}),(req,res)=>{
-    req.flash('succes_msg','you are logged in')
-    res.redirect('/places')
-});
+}),AuthController.login);
 
 
-router.post('/logout',(req,res)=>{
-    req.logout(function(err){
-if(err){ return next(err)
-}
-req.flash('succes_msg', 'you are logged out')
-res.redirect('/login')
-    })
-})
+router.post('/logout', AuthController.logout)
 
 module.exports= router
--- routes/reviews.js
+++ routes/reviews.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const Place = require('../models/place');
 const Review = require('../models/review')
+const ReviewsControler = require('../controllers/reviews')
 const {reviewSchema} =require('../schemas/review')
 const wrapAsync = require('../utils/wrapAsync')
 const ErrorHandler= require('../utils/ErrorHandler')
@@ -22,27 +23,10 @@ const validateReview = (req,res,next) =>{
 }
 
 
-router.post('/',isAuth,isValidObjectId('/places'),validateReview, wrapAsync(async (req,res)=>{
-  const {place_id} = req.params
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    await review.save();
-
-    const place = await Place.findById(req.params.place_id)
-    place.reviews.push(review)
-    await place.save();
-    req.flash('success_msg','Komentar berhasil di tambahkan')
-    res.redirect(`/places/${req.params.place_id}`)
-}))
+router.post('/',isAuth,isValidObjectId('/places'),validateReview, wrapAsync(ReviewsControler.store))
 
 // hapus koment 
-router.delete('/:review_id',isAuthorReview,isAuth,isValidObjectId('/places'), wrapAsync(async(req,res)=>{
-    const {place_id,review_id} = req.params;
-    await Place.findByIdAndUpdate(place_id,{$pull: {reviews: { _id:review_id}}});
-    await Review.findByIdAndDelete(review_id)
-    req.flash('success_msg','komentar berhasil di hapus')
-    res.redirect(`/places/${place_id}`);
-}))
+router.delete('/:review_id',isAuthorReview,isAuth,isValidObjectId('/places'), wrapAsync(ReviewsControler.destroy))
 
 
 module.exports = router;
